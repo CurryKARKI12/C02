bc36aa400e90a02faf7255f141fd60500af32b20
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-23 15:31:06 +0200

    Delete hello_world.c

--- hello_world.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h> //indication du programme en language C et bib
-//
-int main() {
-    printf("Hello, World! \n");
-    return 0;
-}
-//ce programme affiche "Hello World a l'ecran"
