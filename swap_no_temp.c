ed7fc7695f023277f5b790e33848c7f847a302f7
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-24 00:54:40 +0100

    Add files via upload

---
+++ swap_no_temp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a , b;
+    
+    scanf("%d %d", &a, &b); // scanf permet de recuperer et de stocker l'entrée de l'utilisateur 
+//commencons maintenant l'inversion avec une simple règle mathématique
+ a = a + b;
+ b = a - b;
+ a = a - b;
+ printf("Apres echange : a = %d, b = %d\n", a, b);
+ return 0;
+}
