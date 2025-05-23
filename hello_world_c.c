348dae05c120ae8fddd9f5b817d87ddf5edfe7c4
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-23 14:32:55 +0100

    Add files via upload

---
+++ hello_world_c.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //indication du programme en language C et bib
+//
+int main() {
+    printf("Hello, World! \n");
+    return 0;
+}
+//ce programme affiche "Hello World a l'ecran"
