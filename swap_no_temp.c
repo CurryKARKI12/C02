458203d4d1504195bbec603afd6827047ef2ad09
CurryKARKI12 <azerati.1234@gmail.com>
2025-05-28 15:15:13 +0200

    Update swap_no_temp.c

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int a , b;
-
+    int a, b;
     scanf("%d %d", &a, &b);// lire lentrée de lutilisateur 
-// Inversion sans variable temporaire
+
     a = a + b;
     b = a - b;
     a = a - b;
     
-    printf("Après échange: a = %d, b = %d", a, b);
+    printf("Après échange: a = %d, b = %d\n", a, b);
     return 0;
 }
-// créez un programme qui lit deux entiers et les échanges sans utiliser de variablle
